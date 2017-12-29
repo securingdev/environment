d283ff2e7e311f84018d2064d800107a69441eb2
K. M. Hoodlet <khoodlet@gmail.com>
2017-12-29 09:40:23 -0500

    Update srcServer for use with WebPack

--- javascript/buildScripts/srcServer.js
+++ javascript/buildScripts/srcServer.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack, { Compiler } from 'webpack';
+import config from '../webpack.config.dev';
 
-var port = 3000;
-var app = express();
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '../src/index.html'));
