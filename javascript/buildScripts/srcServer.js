55e194b5f85333613f566e7718445f2baceef6fd
K. M. Hoodlet <khoodlet@gmail.com>
2018-01-04 16:07:23 -0500

    Remove compiler; add 'eslint-disable no-console'

--- javascript/buildScripts/srcServer.js
+++ javascript/buildScripts/srcServer.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
-import webpack, { Compiler } from 'webpack';
+import webpack from 'webpack';
 import config from '../webpack.config.dev';
+
+/* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
