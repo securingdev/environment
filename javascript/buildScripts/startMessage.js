dfa295333a0cbc91807b0f2aec3f9769c157151f
K. M. Hoodlet <khoodlet@gmail.com>
2018-01-04 16:06:43 -0500

    Add eslint-disable

--- javascript/buildScripts/startMessage.js
+++ javascript/buildScripts/startMessage.js
@@ -1,3 +1,5 @@
 import chalk from 'chalk';
 
+/* eslint-disable */
+
 console.log(chalk.green('Starting app in dev mode...'));
--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,4 +1,6 @@
 import numeral from 'numeral';
 
+/* eslint-disable */
+
 const value = numeral(1000).format('$0,0.00');
 console.log(`${value}`);
