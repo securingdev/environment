a6118309726b27699f0a88a269a855ea02d0b62b
K. M. Hoodlet <khoodlet@gmail.com>
2018-01-06 13:30:15 -0500

    Add mock API data for Fetch

--- javascript/buildScripts/srcServer.js
+++ javascript/buildScripts/srcServer.js
@@ -19,6 +19,16 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+
+app.get('/users', function(req, res) {
+    // Hardcoded formock API calls
+    res.json([
+        { "id": 1, "firstName": "Alice", "lastName": "Smith", "email": "alice@gmail.com" },
+        { "id": 2, "firstName": "Bob", "lastName": "Smith", "email": "bob@yahoo.com" },
+        { "id": 3, "firstName": "Eve", "lastName": "Smith", "email": "lee@aol.com" }
+    ]);
+});
+
 app.listen(port, function(err){
     if (err) {
         console.log(err);
