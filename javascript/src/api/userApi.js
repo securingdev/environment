cc744a76de1215500050f6dc8a822bd462b0df91
K. M. Hoodlet <khoodlet@gmail.com>
2018-01-08 07:28:32 -0500

    Add 'deleteUser' Mock API call

--- javascript/src/api/userApi.js
+++ javascript/src/api/userApi.js
@@ -7,8 +7,20 @@ export function getUsers() {
     return get('users');
 }
 
+export function deleteUser(id) {
+    return del(`users/${id}`);
+}
+
 function get(url) {
     return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+function del(url) {
+    const request = new Request(baseUrl + url, {
+        method: 'DELETE'
+    });
+
+    return fetch(request).then(onSuccess, onError);
 }
 
 function onSuccess(response) {
--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,4 +1,4 @@
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 
 getUsers().then(result => {
     let usersBody = "";
@@ -14,4 +14,16 @@ getUsers().then(result => {
     });
 
     global.document.getElementById('users').innerHTML = usersBody;
+
+    const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+    Array.from(deleteLinks, link => {
+        link.onclick = function(event) {
+            const element = event.target;
+            event.preventDefault();
+            deleteUser(element.attributes["data-id"].value);
+            const row = element.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        };
+    });
 });
