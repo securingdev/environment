b2f8c7bb09592c17feb71d714889200cef36a872
K. M. Hoodlet <khoodlet@gmail.com>
2018-01-09 08:35:45 -0500

    Update comment spacing

--- javascript/buildScripts/srcServer.js
+++ javascript/buildScripts/srcServer.js
@@ -21,7 +21,7 @@ app.get('/', function(req, res){
 
 
 app.get('/users', function(req, res) {
-    // Hardcoded formock API calls
+    // Hardcoded for mock production API calls
     res.json([
         { "id": 1, "firstName": "Alice", "lastName": "Smith", "email": "alice@gmail.com" },
         { "id": 2, "firstName": "Bob", "lastName": "Smith", "email": "bob@yahoo.com" },
